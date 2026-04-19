ca7f09ec5ba264f1d691cb6ccecd53cbe5210caf
Michael <michaelkivo977@gmail.com>
2026-04-19 19:20:51 +0300

    upload button redesign

--- app/src/main/java/com/example/meetings/presentation/fragments/UploadAudioFragment.kt
+++ app/src/main/java/com/example/meetings/presentation/fragments/UploadAudioFragment.kt
@@ -13,6 +13,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -84,6 +85,9 @@ class UploadAudioFragment : Fragment() {
         setupObservers()
         setupClickListeners()
         setupTextWatchers()
+
+        val toolbarTitle = requireActivity().findViewById<TextView>(R.id.toolbar_title)
+        toolbarTitle.text = "Загрузить аудио встречи"
     }
 
     private fun setupObservers() {
